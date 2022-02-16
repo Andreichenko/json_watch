e37d7adc7f086348d1b97099d98100fb76c74679
andreichenko <andreichenko@web.de>
2022-02-17 01:02:37 +0300

    BackendServer has been created

--- src/main/java/BackendServer.java
+++ src/main/java/BackendServer.java
@@ -1,2 +1,7 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class BackendServer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(BackendServer.class);
+
 }
