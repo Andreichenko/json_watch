8bd49629232008b413ee6560159780a096993cc1
andreichenko <andreichenko@web.de>
2022-04-26 00:03:19 +0300

    BackendServer has been created

--- src/main/java/BackendServer.java
+++ src/main/java/BackendServer.java
@@ -1,7 +1,20 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.ServerSocket;
+import java.util.concurrent.ExecutorService;
+
 public class BackendServer {
     private static final Logger LOGGER = LoggerFactory.getLogger(BackendServer.class);
 
+    private final int port;
+    private Delegate delegate;
+
+    private boolean started;
+    private ExecutorService executor;
+    private ServerSocket serverSocket;
+
+    public BackendServer(int port) {
+        this.port = port;
+    }
 }
