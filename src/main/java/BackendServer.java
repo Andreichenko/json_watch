beacd754f530c4e9096d6f88729b71326273cfb0
andreichenko <andreichenko@web.de>
2022-04-26 01:32:08 +0300

    BackendServer has been created

--- src/main/java/BackendServer.java
+++ src/main/java/BackendServer.java
@@ -3,6 +3,7 @@ import org.slf4j.LoggerFactory;
 
 import java.net.ServerSocket;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class BackendServer {
     private static final Logger LOGGER = LoggerFactory.getLogger(BackendServer.class);
@@ -13,6 +14,8 @@ public class BackendServer {
     private boolean started;
     private ExecutorService executor;
     private ServerSocket serverSocket;
+
+    private AtomicInteger clientIDSequence = new AtomicInteger(1);
 
     public BackendServer(int port) {
         this.port = port;
